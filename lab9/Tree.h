d890721b8d9350262ec725e069cdd83e12888c8e
Иван Глеков < @DESKTOP-96VCV86>
2024-05-15 19:05:41 +0300

    Добавьте файлы проекта.

---
+++ lab9/Tree.h
@@ -0,0 +1,161 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Tree
+{
+public:
+	struct Node
+	{
+		int data;
+		Node* left;
+		Node* right;
+		Node(int data) : data(data), left(nullptr), right(nullptr) {};
+	};
+	Tree();
+	~Tree();
+	void add(int data);
+	void remove(int data);
+	bool contains(int data);
+	int count();
+	void clear();
+	Node* getRoot();
+private:
+	int size;
+	Node* root;
+	void clearnode(Node* node)
+	{
+		if (node == nullptr)
+		{
+			return;
+		}
+		clearnode(node->left);
+		clearnode(node->right);
+		delete node;
+	}
+
+	Node* deleteByMerging(Node*& node) {
+		Node* tmp = node;
+		if (node != nullptr) {
+			if (!node->right) node = node->left;
+			else if (node->left == nullptr) node = node->right;
+			else {
+				tmp = node->left;
+				while (tmp->right != nullptr) tmp = tmp->right;
+				tmp->right = node->right;
+				tmp = node;
+				node = node->left;
+			}
+			delete tmp;
+			size--;
+			return node;
+		}
+	}
+};
+Tree::Tree()
+{
+	size = 0;
+	root = nullptr;
+}
+
+Tree::~Tree()
+{
+	clear();
+}
+
+void Tree::add(int data)
+{
+	if (contains(data))
+	{
+		return;
+	}
+	Node* newnode = new Node(data);
+	if (root == nullptr)
+	{
+		root = newnode;
+	}
+	else
+	{
+		Node* current = root;
+		while (true)
+		{
+			if (data < current->data)
+			{
+				if (current->left == nullptr)
+				{
+					current->left = newnode;
+					break;
+				}
+				current = current->left;
+			}
+			else
+			{
+				if (current->right == nullptr)
+				{
+					current->right = newnode;
+					break;
+				}
+				current = current->right;
+			}
+		}
+	}
+	size++;
+}
+
+void Tree::remove(int data) {
+	Node* node = root, * prev = 0;
+	while (node != nullptr) {
+		if (node->data == data)
+			break;
+		prev = node;
+		if (data < node->data)
+			node = node->left;
+		else node = node->right;
+	}
+	if (node != nullptr && node->data == data) {
+		if (node == root)
+			root = deleteByMerging(root);
+		else if (prev->left == node)
+			prev->left = deleteByMerging(prev->left);
+		else
+			prev->right = deleteByMerging(prev->right);
+	}
+}
+
+bool Tree::contains(int data)
+{
+	Node* current = root;
+	while (current != nullptr)
+	{
+		if (current->data == data)
+		{
+			return true;
+		}
+		if (data < current->data)
+		{
+			current = current->left;
+		}
+		else
+		{
+			current = current->right;
+		}
+	}
+	return false;
+}
+
+int Tree::count()
+{
+	return size;
+}
+
+void Tree::clear()
+{
+	clearnode(root);
+	root = nullptr;
+	size = 0;
+}
+
+Tree:: Node* Tree::getRoot()
+{
+	return root;
+}
---
+++ lab9/lab9.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <string>
+#include "Tree.h"
+
+class HashTree 
+{
+private:
+	static const int size = 503;
+	Tree hashTable[size];
+	int hashFunc(int key) const
+	{
+		unsigned long hash = 0;
+		hash = (hash * 37 + key) % size;
+		return hash % size;
+	}
+public:
+	HashTree();
+	~HashTree();
+	void add(int data);
+	void remove(int data);
+	void clear();
+	bool contains(int data);
+	int count();
+	int* ToArray();
+	void getTreeElements(Tree::Node* node, int* array, int& index);
+	
+};
+
+HashTree::HashTree()
+{
+}
+
+HashTree::~HashTree()
+{
+	clear();
+}
+
+void HashTree::add(int data)
+{
+	int index = hashFunc(data);
+	hashTable[index].add(data);
+}
+
+void HashTree::remove(int data)
+{
+	int index = hashFunc(data);
+	hashTable[index].remove(data);
+}
+
+void HashTree::clear()
+{
+	for (int i = 0; i < size; i++)
+	{
+		hashTable[i].clear();
+	}
+}
+
+bool HashTree::contains(int data)
+{
+	int index = hashFunc(data);
+	return hashTable[index].contains(data);
+}
+
+
+int HashTree::count()
+{
+	int total = 0;
+	for (int i = 0; i < size; i++)
+	{
+		total += hashTable[i].count();
+	}
+	return total;
+}
+
+int* HashTree::ToArray()
+{
+	int total = count();
+	int* array = new int[total];
+	int index = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		int treeSize = hashTable[i].count();
+		getTreeElements(hashTable[i].getRoot(), array, index);
+	}
+	return array;
+}
+
+void HashTree::getTreeElements(Tree::Node* node, int* array, int& index)
+{
+	if (node == nullptr) return;
+	getTreeElements(node->left, array, index);
+	array[index++] = node->data;
+	getTreeElements(node->right, array, index);
+}
+
+int main() 
+{
+	return 0;
+}
